b3428aee33e66969ec3f262fa6c39d9f831c98d6
denis <denis@gosweat.com>
2019-11-30 23:53:24 +0000

    in the middle of downloading the image

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const got = require("got");
 const geolib = require("geolib");
-
+const childProcess = require("child_process");
+const https = require('https');
+const fs = require('fs');
 
 const getFeedList = async () => {
   const info = await got("https://api.tfl.gov.uk/Place/Type/JamCam/?app_id=a20e2f12&app_key=732436bb1747a061ed9ec411023b9315", { json: true });
@@ -27,11 +29,33 @@ const getFeatureCoordRange = async () => {
 
 const app = express();
 
-const getTakenUpParkingSpaceInM = (imageUrl, featureCoordRange) => {
-  const cars = 3;
-  const minivans = 2;
-  const spaceTakenUpInM = cars * 4.5 + minivans * 6.7
-  return spaceTakenUpInM;
+const saveImageFile = (imageUrl) => {
+  return new Promise((resolve, reject) => {
+  const file = fs.createWriteStream("road.jpg");
+  console.log(imageUrl);
+  https.get(imageUrl, (response) => {
+    response.pipe(file);
+    
+  }).on('finish', function() {
+    file.close(cb);
+    resolve(true);
+  })
+})
+};
+
+const getTakenUpParkingSpaceInM = (featureCoordRange) => {
+  return new Promise((resolve, reject) => {
+    try{
+      const pythonProcess = childProcess.spawn('python',["script.py"]);
+      pythonProcess.stdout.on('data', (data) => {
+        const {cars, minivans} = JSON.parse(data);
+        const spaceTakenUpInM = cars * 4.5 + minivans * 6.7
+        resolve(spaceTakenUpInM)
+      });
+    } catch (err){
+      reject(err);
+    }
+  })
 };
 
 app.get('/api/feature/:featureid/space-probability', async (req, res) => {
@@ -39,9 +63,10 @@ app.get('/api/feature/:featureid/space-probability', async (req, res) => {
   const featureCoordRange = await getFeatureCoordRange(featureid);
   const { body: allCameraFeeds} = await getFeedList();
   const closestFeed = await findClosestFeed(featureCoordRange, allCameraFeeds);
-  const feedImageUrl = closestFeed.additionalProperties.filter(props => props.key === "imageUrl")[0];
+  const feedImageUrl = closestFeed.additionalProperties.filter(props => props.key === "imageUrl")[0].value;
   const lengthOfFeature = geolib.getDistance(...featureCoordRange);
-  const spaceTakenUpInM = getTakenUpParkingSpaceInM(feedImageUrl, featureCoordRange);
+  const isImageSaved = await saveImageFile(feedImageUrl);
+  const spaceTakenUpInM = await getTakenUpParkingSpaceInM(featureCoordRange);
   const remainingLengthInFeature = lengthOfFeature-spaceTakenUpInM;
   const remainingSpaces = Math.floor(remainingLengthInFeature/4.5);
   res.status(200).json({remainingSpaces, lengthOfFeature, spaceTakenUpInM});
